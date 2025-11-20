e1f0c62e2e5d733f46415b1851d2976a364f379e
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-20 13:08:09 +0200

    Add a background image and change navigation links

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -6,7 +6,8 @@ export default function Navigation() {
             <Link className="font-bold text-xl tracking-tight" to="/">Book Insider</Link>
             <div className="flex items-center">
                 <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/">Home</Link>
-                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="#">About</Link>
+                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/catalog">Catalog</Link>
+                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="#">Register</Link>
                 <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/login">Login</Link>
             </div>
         </nav>
