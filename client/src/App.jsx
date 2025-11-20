ec0b054daccd900e4d32d0ff531e83cb1aa894d0
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-20 11:33:17 +0200

    Install react-router and basic home and login routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router"
+import Login from "./components/login/Login.jsx"
+import Home from "./components/Home/Home.jsx"
+
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+
+        </>
+    )
 }
 
 export default App
