db8178c5eb1381bab3f8f02df91f4d0355d60e03
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-20 12:14:10 +0200

    Add Navigation Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router"
 import Login from "./components/login/Login.jsx"
 import Home from "./components/Home/Home.jsx"
+import Navigation from "./components/navigation/Navigation.jsx"
 
 function App() {
 
     return (
         <>
+            <Navigation />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
---
+++ client/src/components/navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+export default function Navigation() {
+    return (
+        <nav className="bg-gray-600 text-white py-3 px-4 flex items-center justify-between">
+            <Link className="font-bold text-xl tracking-tight" to="/">Book Insider</Link>
+            <div className="flex items-center">
+                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/">Home</Link>
+                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="#">About</Link>
+                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/login">Login</Link>
+            </div>
+        </nav>
+    )
+}
