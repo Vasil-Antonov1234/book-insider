d651a3613166d7342ba0a89fe72db11ce9020294
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-28 00:50:11 +0200

    Update Footer.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -9,11 +9,15 @@ export default function Footer() {
             </p>
 
             <span className="block text-sm text-center text-gray-500">© 2025 Book Insider Built with
-                <a href="https://react.dev/"
-                    className="text-blue-500 hover:underline"> React</a> and
-                <a
-                    href="https://tailwindcss.com" className="text-blue-500 hover:underline"> Tailwind CSS
-                </a>.
+                <Link to="https://react.dev/"
+                    className="text-blue-500 hover:underline"> React</Link>,
+                <Link
+                    to="https://tailwindcss.com" className="text-blue-500 hover:underline"> Tailwind CSS
+                </Link>  and
+                <Link
+                    to={"https://github.com/softuni-practice-server/softuni-practice-server"} 
+                    className="text-blue-500 hover:underline"> SoftUni Practice Server
+                </Link>
             </span>
         </div>
     );
