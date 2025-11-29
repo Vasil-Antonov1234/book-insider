71c6cfd041f88cbecb8f0fe2341aa2b8d5da2592
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-30 00:49:43 +0200

    Show message when there are no books in Home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -13,13 +13,22 @@ export default function Home() {
 
     return (
         <div>
-            <div className="h-34 bg-gradient-to-r via-gray-500 mt-4 flex items-center justify-center">
-                <h1 className="font-bold text-6xl tracking-tight font-serif text-white">{ isPending ? "Loading..." : "All your favorite books here"}</h1>
-            </div>
+            
 
-            <section className="flex items-center justify-center space-x-34">
-                {books.map((book) => <BookCard key={book._id} book={book} />)}
-            </section>
+            {books.length ? 
+            <>
+                    <div className="h-34 bg-gradient-to-r via-gray-500 mt-4 flex items-center justify-center">
+                        <h1 className="font-bold text-6xl tracking-tight font-serif text-white">{isPending ? "Loading..." : "All your favorite books here"}</h1>
+                    </div>
+                    <section className="flex items-center justify-center space-x-34">
+                        {books.map((book) => <BookCard key={book._id} book={book} />)}
+                    </section> 
+            </> :
+                    <section className="flex items-center justify-center space-x-34 mt-91 mb-59">
+                        <h1 className="font-bold text-6xl tracking-tight font-serif text-black">There are no books yet</h1>
+                    </section>
+            }
+
 
             <Footer />
 
