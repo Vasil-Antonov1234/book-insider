1b5a7166e439d12cb39c644b8319f3d374d77be0
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-12-03 19:48:49 +0200

    Show user email in header when authenticated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,25 +3,31 @@ import { Link } from "react-router";
 import UserContext from "../../contexts/UserContext.jsx";
 
 export default function Header() {
-    const { isAuthenticated } = useContext(UserContext)
+    const { isAuthenticated, user } = useContext(UserContext)
 
     return (
-        <nav className="bg-gray-600 text-white py-3 px-4 flex items-center justify-between">
-            <Link className="font-bold text-xl tracking-tight" to="/">Book Insider</Link>
-            <div className="flex items-center">
-                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/">Home</Link>
-                <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/catalog">Catalog</Link>
-                {isAuthenticated ?
-                    <>
-                        <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/create">Create Book</Link>
-                        <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to={"/logout"}>Logout</Link>
-                    </> :
-                    <>
-                        <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/register">Register</Link>
-                        <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/login">Login</Link>
-                    </>
-                }
+        <nav>
+            <div className="bg-gray-600 text-white py-3 px-4 flex items-center justify-between">
+                <Link className="font-bold text-xl tracking-tight" to="/">Book Insider</Link>
+                <div className="flex items-center">
+                    <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/">Home</Link>
+                    <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/catalog">Catalog</Link>
+                    {isAuthenticated ?
+                        <>
+                            <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/create">Create Book</Link>
+                            <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to={"/logout"}>Logout</Link>
+                        </> :
+                        <>
+                            <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/register">Register</Link>
+                            <Link className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" to="/login">Login</Link>
+                        </>
+                    }
+                </div>
+            </div>
+            <div className="flex justify-end">
+            {isAuthenticated && <span className="text-sm px-4 py-2 leading-none rounded-full font-bold">Welcome {user.email}</span>}
             </div>
         </nav>
+
     )
 }
